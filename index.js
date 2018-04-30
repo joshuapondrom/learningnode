4297e26db2293c4de23b6b8624730f39f413758f
Joshua Pondrom <japrkf@mst.edu>
2018-04-30 15:49:48 -0500

    moved basics to a different directory. index is now a http server

--- index.js
+++ index.js
@@ -1,47 +1,17 @@
-// Simple print
-console.log("hello");
+const http = require('http')
+const port = 3000
 
-// Importing a node module
-const _ = require('lodash');
-
-
-// Map with lodash
-mymap = _.assign({'one':1}, {'two':2}, {'three':3});
-console.log(mymap['one']);
-
-// Normal file streaming
-const fs = require('fs');
-let content;
-try {
-    content = fs.readFileSync('file.md', 'utf-8');
-} catch (ex) {
-    console.log(ex);
-}
-console.log(content);
-
-// Functions that take functions as arguments are higher-order functions
-// This is because functions are first-class citizens
-const numbers = [1,2,3,4,5];
-
-function isEqualToTwo (num) {
-    return num === 2;
+const requestHandler = (request, response) => {
+    console.log(request.url)
+    response.end('Hello!')
 }
 
-arrayOfTwos = numbers.filter(isEqualToTwo);
-console.log(arrayOfTwos);
+const server = http.createServer(requestHandler)
 
-// If you pass a function to another function, you can call it within the function when you are finished with your job
-// This is called callbacks
-// 
-// File streaming with callbacks
-// Notice there is error checking in callback, rather than with a try-catch
-// Also note that there is no return value, those are passed to the callbacks
-fs.readFile('file.md', 'utf-8', function (err, content) {
+server.listen(port, (err) => {
     if (err) {
-        return console.log(err);
+        return console.log('whoops', err)
     }
 
-    console.log(content);
+    console.log(`server is listening on ${port}`)
 })
-
-console.log('End of file');
---
+++ src/basics.js
@@ -0,0 +1,74 @@
+// Simple print
+console.log("hello");
+
+// Importing a node module
+const _ = require('lodash');
+
+
+// Map with lodash
+mymap = _.assign({'one':1}, {'two':2}, {'three':3});
+console.log(mymap['one']);
+
+// Normal file streaming
+const fs = require('fs');
+let content;
+try {
+    content = fs.readFileSync('file.md', 'utf-8');
+} catch (ex) {
+    console.log(ex);
+}
+console.log(content);
+
+// Functions that take functions as arguments are higher-order functions
+// This is because functions are first-class citizens
+const numbers = [1,2,3,4,5];
+
+function isEqualToTwo (num) {
+    return num === 2;
+}
+
+arrayOfTwos = numbers.filter(isEqualToTwo);
+console.log(arrayOfTwos);
+
+// If you pass a function to another function, you can call it within the function when you are finished with your job
+// This is called callbacks
+// 
+// File streaming with callbacks
+// Notice there is error checking in callback, rather than with a try-catch
+// Also note that there is no return value, those are passed to the callbacks
+fs.readFile('file.md', 'utf-8', function (err, content) {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log(content);
+})
+
+console.log('End of file');
+
+// Async.js is used to avoid 'Callback-Hell'
+// It helps structure applications and simplifies control flow
+const async = require("async");
+async.each(['file1','file2','file3'], fs.stat, function(err, results) {
+
+})
+
+// The previous example can be written with promises too
+function stats(file) { 
+    return new Promise((resolve, reject) => {
+        fs.stat(file, (err, data) => {
+            if (err) {
+                return reject (err)
+            }
+            resolve(data)
+        })
+    })
+}
+
+Promise.all([
+    stats('file1'),
+    stats('file2'),
+    stats('file3')
+])
+.then((data) => console.log(data))
+.catch((err) => console.log(err))
