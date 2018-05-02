429a78dfef1c8531178c37090699dd81f319463c
Joshua Pondrom <japrkf@mst.edu>
2018-05-02 18:24:22 -0500

    changed index back to a express framework

--- src/basics.js
+++ src/basics.js
@@ -74,3 +74,14 @@ Promise.all([
 ])
 .then((data) => console.log(data))
 .catch((err) => console.log(err))
+
+// Arrow functions
+function myprint (word, printfn) {
+    printfn(word);
+}
+
+myprint('hello', (x) => console.log(x));
+
+let testNumbers = [1,1,2,3,5,8,13];
+testNumbers = testNumbers.map((element) => element * 2);
+console.log(testNumbers);
