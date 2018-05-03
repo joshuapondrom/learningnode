522404df310aec75c608e450d8dbf93ec2db36d7
Joshua Pondrom <japrkf@mst.edu>
2018-05-03 12:57:47 -0500

    added discord bot

--- index.js
+++ index.js
@@ -1,35 +1,12 @@
-// const PIXI =  require("pixi.js");
+const discord = require('discord.js');
+const client = new discord.Client();
 
-const express = require('express');
-const app = express();
-const port = 3000;
+const config = require('./config.json');
 
-const path = require('path');
-
-// For html files
-app.use(express.static(path.join(__dirname, 'views')));
-// For js and css
-app.use(express.static(path.join(__dirname, 'scripts')));
-
-app.get('/', (request, response) => {
-    response.send('Hello!');
-})
-
-app.get('/echo/:urlString', (request, response) => {
-    response.send(request.params['urlString']);
-})
-
-app.get('/pixi', (request, response) => {
-    response.sendFile(path.join(__dirname, 'views', 'pixi.html'));
-})
-
-app.get('/secret', (request, response) => {
-    response.send('Secret page');
-})
-
-app.listen(port, (err) => {
-    if (err) {
-        return console.log('whoops', err);
+client.on('message', message => {
+    if (message.content ===  '!ping') {
+        message.channel.send('Pong.');
     }
-    console.log(`server is listening on ${port}`);
-})
+});
+
+client.login(config.token);
--- src/express.js
+++ src/express.js
@@ -1,15 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+// const PIXI =  require("pixi.js");
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const path = require('path');
+
+// For html files
+app.use(express.static(path.join(__dirname, 'views')));
+// For js and css
+app.use(express.static(path.join(__dirname, 'scripts')));
 
 app.get('/', (request, response) => {
-    response.send('Hello!')
+    response.send('Hello!');
+})
+
+app.get('/echo/:urlString', (request, response) => {
+    response.send(request.params['urlString']);
+})
+
+app.get('/pixi', (request, response) => {
+    response.sendFile(path.join(__dirname, 'views', 'pixi.html'));
+})
+
+app.get('/secret', (request, response) => {
+    response.send('Secret page');
 })
 
 app.listen(port, (err) => {
     if (err) {
-        return console.log('whoops', err)
+        return console.log('whoops', err);
     }
-
-    console.log(`server is listening on ${port}`)
+    console.log(`server is listening on ${port}`);
 })
